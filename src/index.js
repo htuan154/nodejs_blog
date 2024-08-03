b2bbf6cbfe8a01c758ecb61987777b0434c8f336
htuan <htuan15424@gmail.com>
2024-08-03 10:03:38 +0700

    Prettier Code formatter

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const exphbs = require('express-handlebars');
 const route = require('./routes');
 
 // Middleware xử lý form data
-   app.use(express.urlencoded({ extended: true }));
-                  app.use(express.json({ limit: 10 }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: 10 }));
 // Static file
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP Logger
 app.use(morgan('combined'));
 
 // Template engine
-              app.engine(
+app.engine(
     'hbs',
     exphbs.engine({
         //Rename
